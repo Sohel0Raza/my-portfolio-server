02d9b37f4d2f64af25329933c97b60ede4c8a646
Sohel0Raza <soheldot1239@gmail.com>
2024-08-10 16:38:35 +0600

    set up the project and connect mongoose

---
+++ index.js
@@ -0,0 +1,27 @@
+import express from "express";
+import dotenv from "dotenv";
+import cors from "cors";
+import mongoose from "mongoose";
+
+dotenv.config();
+const app = express();
+
+const port = process.env.PORT || 5000;
+
+//middleware
+app.use(cors());
+app.use(express.json());
+
+const uri = `mongodb+srv://${process.env.DB_USER}:${process.env.DB_PASS}@cluster0.q6zwl04.mongodb.net/portfolioDB?retryWrites=true&w=majority&appName=Cluster0`;
+
+async function run() {
+  await mongoose.connect(uri);
+}
+
+run()
+  .then(() => console.log("mongoose connect successfuly"))
+  .catch((error) => console.log(error));
+
+  app.listen(port ,()=>{
+    console.log(`my portfolio server is running on port:${port}`)
+  })
